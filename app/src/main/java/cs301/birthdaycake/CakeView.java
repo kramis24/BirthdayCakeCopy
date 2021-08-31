70d64f29e56a205b140744d81a6847ec1157c021
kramis24 <dylancray777@gmail.com>
2021-08-31 16:44:35 -0700

    Added a switch that does nothing and another candle

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -119,8 +119,9 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        //Now two candles, spaced equidistant from each other and the cake's edges
+        drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft + (2*cakeWidth)/3 - candleWidth/2, cakeTop);
 
     }//onDraw
 
